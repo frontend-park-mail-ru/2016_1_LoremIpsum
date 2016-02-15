7db1179e0041f560fb4f962927a8d4e377dd6f00
Vertaler <dancheran@yandex.ru>
2016-02-16 00:29:32 +0300

    Update server.js
    Внесены незначительные изменения, связанные с оформлением кода.

--- server.js
+++ server.js
@@ -3,23 +3,23 @@ var express = require('express'),
     app = express();
 
 var HOSTNAME = 'localhost',
-    PORT = 3280,
+    PORT = 8080,
     PUBLIC_DIR = __dirname + '/public_html';
 
-var counter = 0;
+var request_counter = 0;
 app.use(function (req, res, next) {
 	// Здесь нужно написать журналирование в формате
 	// (журналирование - вывод в консоль)
 	// [время] [номер запроса по счету]
-	var request_time =new Date();
-	console.log('Time: ',request_time.toString());
-	console.log("Requests count: " , ++counter);
+	var request_time = new Date();
+	console.log('Time: ', request_time.toString());
+	console.log("Requests count: ", ++request_counter);
 	next();
 
 });
- app
+app
 	.use('/', express.static(PUBLIC_DIR))
-	 .use(errorHandler());
+	.use(errorHandler());
 
 app.listen(PORT, function () {
 	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
