a42a521765599dc900567bbf0ae63200aa0534ae
just <you@localhost.localdomain>
2016-02-15 01:18:43 -0100

    Static Hello World. Date and request counter

--- server.js
+++ server.js
@@ -2,20 +2,29 @@ var express = require('express'),
     errorHandler = require('errorhandler'),
     app = express();
 
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
 var HOSTNAME = 'localhost',
     PORT = 8080,
     PUBLIC_DIR = __dirname + '/public_html';
 
-app.use(function (req) {
+var reqCounter = 0;
+
+app.use(function (req, res, cb) {
 	// Здесь нужно написать журналирование в формате
 	// (журналирование - вывод в консоль)
 	// [время] [номер запроса по счету]
+	var date = new Date();
+	console.log("[" + date + "]" + " | " + "request number " + ++reqCounter)
+	cb()
 });
 
 app
-	.use('/', express.static(PUBLIC_DIR))
-	.use(errorHandler());
+	.use('/', express.static(PUBLIC_DIR)) 
+	.use(errorHandler()); 
 
 app.listen(PORT, function () {
 	console.log("Simple static server showing %s listening at http://%s:%s", PUBLIC_DIR, HOSTNAME, PORT);
 });
+
